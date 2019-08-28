e240ad2b3fe6eba8a49c9c07c2e7ea8ac95522f9
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-08-28 12:28:26 -0400

    Update RC-Communication.ino
    Fixing bugs that prevented the Arduino from recognizing input from the pi. The pi is now sending a string and the Arduino uses the parseInt() function to change the string into an integer.

--- Arduino/RC-Communication.ino
+++ Arduino/RC-Communication.ino
@@ -4,8 +4,8 @@ const int rf= 2;
 const int rb= 3;
 const int lf= 4;
 const int lb= 5;
-int incomingByte;
-
+int incomingByte = 0;
+int counter = 0;
 
 
 void setup() {
@@ -22,19 +22,31 @@ void loop() {
    digitalWrite(rb, HIGH);
     digitalWrite(lf, HIGH);
       digitalWrite(lb, LOW);
-      Serial.println("ON");
+      Serial.println("ON"); // Send message to the raspberry pi that the car is moving
       if(Serial.available()>0)
      
       {
-        incomingByte=Serial.read();
-        if(incomingByte==1)
+        incomingByte=Serial.parseInt();
+        if(incomingByte == 1)
         {
         digitalWrite(rf, LOW); // Complete stop for 2 Seconds
-      digitalWrite(rb, LOW);
-       digitalWrite(lf, LOW);
+        digitalWrite(rb, LOW);
+        digitalWrite(lf, LOW);
         digitalWrite(lb, LOW);
         Serial.println("OFF");
         delay(2000);
+         
+        // Then move ahead out of the range of the stop sign.
+        for(counter = 0; counter < 10; counter++)
+        {
+             // Move forward
+             digitalWrite(rf, LOW); //Moving Forward
+             digitalWrite(rb, HIGH);
+             digitalWrite(lf, HIGH);
+             digitalWrite(lb, LOW);
+             Serial.println("ON"); 
+            
+        }
         }
         Serial.flush();
         
